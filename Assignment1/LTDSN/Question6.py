d450102b5b3cc1c8c05e4d43b049953154ab1c51
Manas Ranjan Mohanta <manasranjanmohanta679@gmail.com>
2023-03-03 12:03:22 +0530

    Did Question6

---
+++ Assignment1/LTDSN/Question6.py
@@ -0,0 +1,6 @@
+# 6. Write a python program to store names of 10 fruits in strings and sort in 
+# alphabetical order
+fruits=['mango','apple','guava','bannana','orange','lemon','pine apple','grapes','watermelon','cherry']
+print("String before sorted: " , fruits)
+fruits.sort()
+print("String after sorted: " , fruits)
